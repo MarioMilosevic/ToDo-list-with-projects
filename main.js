8a34d5578e756c15602e307e57b89935ddc67219
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-15 16:17:19 +0100

    Make input and buttons div

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const projects = document.querySelector('.projects')
 const todos = document.querySelector('.todos')
 const addProjectBtn = document.querySelector('.addProject')
 const addTodoBtn = document.querySelector('.addTodo')
-
+const projectsInputDiv = document.querySelector('.projectsInput')
 
 addProjectBtn.addEventListener('click', function(){
-    
+    addProjectBtn.classList.add('hidden')
+    projectsInputDiv.classList.remove('hidden')
 })
+
+// const createInput = () => {
+//     const input = document.create
+// }
