0a7f440bb8042f2935ec2a6e4af290a761172177
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-15 15:47:05 +0100

    Select query selectors

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict'
+const projects = document.querySelector('.projects')
+const todos = document.querySelector('.todos')
+const addProjectBtn = document.querySelector('.addProject')
+const addTodoBtn = document.querySelector('.addTodo')
+
+
+addProjectBtn.addEventListener('click', function(){
+    
+})
